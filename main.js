33a3875b1d74a54433ae5a419bcb3d1353b19518
Ahmad-Hawat <ahmad3lrwaq@gmail.com>
2023-08-11 21:34:23 +0300

    Enhance responsive

---
+++ main.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector("#menuBtn");
+let hul = document.querySelector("#hul");
+const menuHandler = ()=>{
+   hul.classList.toggle("hul")
+   hul.classList.toggle("none")
+}
